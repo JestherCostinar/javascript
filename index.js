9f5b4eec8f992ea0c8f19383fec06804b756c52d
Jesther Costinar <jesther.jc15@gmail.com>
2023-03-02 17:52:24 +0800

    Operator

--- index.js
+++ index.js
@@ -29,13 +29,15 @@ let selectedColors = ['Red', 'Orange', 'Blue'];
 selectedColors[3] = 'Green';
 selectedColors[4] = 2;
 
+// console.log(selectedColors.length);
+
 // Function
 // Performing a task
 function greet(name, lastName) { // Inside the parenthesis is parameter
     console.log('Hello ' + name + ' ' + lastName);
 }
 
-greet('Jesther', 'Costinar'); // Jesther value is called argument
+// greet('Jesther', 'Costinar'); // Jesther value is called argument
 
 // Calculate a value
 function square(number) {
@@ -43,5 +45,87 @@ function square(number) {
 }
 
 let number = square(2);
-console.log(number);
+// console.log(number);
 
+// JavaScript Operator
+
+// Arithmetic operator
+let x = 10;
+let y = 3;
+
+// console.log(x + y); //Addition
+// console.log(x - y); //Subtraion
+// console.log(x * y); //Mulitiplication
+// console.log(x / y); //Division
+// console.log(x % y); //Modulo
+// console.log(x ** y); //Exponent
+
+// console.log(x++); //Increment
+// console.log(x--); //Decrement
+
+// Assignment Operator
+
+x = x + 5;
+x += 5 // Shortcut way
+
+// Compasiron Operator
+
+// Relational Operator
+// console.log(x > 0);
+// console.log(x >= 10);
+// console.log(x < 1);
+// console.log(x <= 1);
+
+// // Equality Operator
+// console.log(x === 0); check if value and data type is equal
+// console.log(x !== 0); check if value and data type is not equal
+// Tips: Most of the time use the strict than the lost because its more precise and accurate
+
+// Ternary Operator
+let points = 110;
+
+let type = points > 100 ? 'Gold' : 'Silver';
+// console.log(type);
+
+// Logical Operator
+
+// Logical AND (&&)
+console.log(true && true); // True - Both operand are True
+console.log(true && true); // False - 1 is False 
+
+let highIncome = true;
+let creditScore = true;
+
+let eligibleForLoan = highIncome && creditScore;
+console.log(eligibleForLoan);
+ 
+// Logical OR (||)
+console.log(true && true); // True - atleast 1 operand is True
+console.log(false && true);
+
+// Not Operator
+let applicationRefused = !eligibleForLoan;
+console.log(applicationRefused);
+
+// Falsy 
+// - underfined
+// - null
+// - 0
+// - false
+// - ''
+// - NaN
+
+// Truthy - Anything that is not Falsy
+// - Number
+// - String
+
+// Swapping Variable
+let a = 'red';
+let b = 'blue';
+
+let temp = a;
+a = b;
+b = temp;
+
+console.log('A', a);
+console.log('B', b);
